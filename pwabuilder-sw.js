fa5f8d2b4fc1aa181ab30c9e5e46142d33a2f14f
Daniel Huerta <dhuertasanchez@gmail.com>
2021-01-27 14:22:17 -0600

    Modificado el builder

--- pwabuilder-sw.js
+++ pwabuilder-sw.js
@@ -1,24 +1,39 @@
-const CACHE = "pwabuilder-offline";
-const QUEUE_NAME = "bgSyncQueue";
+// const CACHE = "pwabuilder-offline";
+// const QUEUE_NAME = "bgSyncQueue";
 
-importScripts('https://storage.googleapis.com/workbox-cdn/releases/5.1.2/workbox-sw.js');
+// importScripts('https://storage.googleapis.com/workbox-cdn/releases/5.1.2/workbox-sw.js');
 
-self.addEventListener("message", (event) => {
-  if (event.data && event.data.type === "SKIP_WAITING") {
-    self.skipWaiting();
-  }
-});
+// self.addEventListener("message", (event) => {
+//   if (event.data && event.data.type === "SKIP_WAITING") {
+//     self.skipWaiting();
+//   }
+// });
 
-const bgSyncPlugin = new workbox.backgroundSync.Plugin(QUEUE_NAME, {
-  maxRetentionTime: 24 * 60 // Retry for max of 24 Hours (specified in minutes)
-});
+// const bgSyncPlugin = new workbox.backgroundSync.Plugin(QUEUE_NAME, {
+//   maxRetentionTime: 24 * 60 // Retry for max of 24 Hours (specified in minutes)
+// });
 
-workbox.routing.registerRoute(
-  new RegExp('/*'),
-  new workbox.strategies.StaleWhileRevalidate({
-    cacheName: CACHE,
-    plugins: [
-      bgSyncPlugin
-    ]
-  })
-);
+// workbox.routing.registerRoute(
+//   new RegExp('/*'),
+//   new workbox.strategies.StaleWhileRevalidate({
+//     cacheName: CACHE,
+//     plugins: [
+//       bgSyncPlugin
+//     ]
+//   })
+// );
+
+const staticDevCovTrack = "cov-track-site-v1"
+const assets = [
+    "/",
+    "index.html",
+    "css/style.css",
+]
+
+self.addEventListener("install", installEvent => {
+    installEvent.waitUntil(
+        caches.open(staticDevCovTrack).then(cache => {
+            cache.addAll(assets)
+        })
+    )
+})
